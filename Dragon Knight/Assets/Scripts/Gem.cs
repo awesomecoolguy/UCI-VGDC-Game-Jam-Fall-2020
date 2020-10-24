5c529cb3beb5bfff2f25ced74d23f734fc9c3162
WhiteWolf94 <prabhurohit02@gmail.com>
2020-10-24 11:49:41 -0700

    Added movement script and gem collection script.

---
+++ Dragon Knight/Assets/Scripts/Gem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var other = collision.gameObject.GetComponent<PlayerController>();
+        if(other != null)
+        {
+            other.AddGemCollected();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Dragon Knight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Configuration Parameters
+    [Header("Movement Parameters")]
+    [SerializeField] private float movementSpeed = 2f;
+    [SerializeField] private float jumpVelocity = 4f;
+
+    private bool onGround = true;
+    private int gemsCollected = 0;
+
+    //Cached references
+    Animator playerAnim;
+    Rigidbody2D playerRB;
+    Collider2D playerCol;
+    Collider2D Ground;
+
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody2D>();
+        playerCol = GetComponent<Collider2D>();
+        Ground = FindObjectOfType<CompositeCollider2D>();
+    }
+
+    void Update()
+    {
+        HorizontalMovement();
+        PlayerJump();
+        DetermineOnGround();
+    }
+
+    private void HorizontalMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            FlipPlayer(1);
+            playerRB.velocity = new Vector2(movementSpeed, playerRB.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            FlipPlayer(-1);
+            playerRB.velocity = new Vector2(-movementSpeed, playerRB.velocity.y);
+        }
+
+        else
+        {
+            playerRB.velocity = new Vector2(0f, playerRB.velocity.y);
+        }
+    }
+
+    private void FlipPlayer(int desiredScale)
+    {
+        if (transform.localScale.x != desiredScale)
+        {
+            transform.localScale = new Vector2(desiredScale, transform.localScale.y);
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+        {
+            playerRB.velocity += new Vector2(0f, jumpVelocity);
+        }
+    }
+
+    private void DetermineOnGround()
+    {
+        if(playerCol.IsTouching(Ground))
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+    }
+
+    public void AddGemCollected()
+    {
+        gemsCollected += 1;
+    }
+}
+
